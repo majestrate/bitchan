ba1c7512436a13532be183f03aaeabc699716c8d
Jeff Becker <jeff@i2p.rocks>
2020-01-04 09:00:39 -0500

    more

--- storage/file.go
+++ storage/file.go
@@ -1,11 +1,16 @@
 package storage
 
+import (
+	"os"
+)
+
 type fileStorage struct {
 	RootDir string
 }
 
 func (f *fileStorage) SetRoot(path string) {
 	f.RootDir = path
+	os.Mkdir(f.RootDir, os.FileMode(0700))
 }
 
 func (f *fileStorage) GetRoot() string {
--- torrent/grabber.go
+++ torrent/grabber.go
@@ -1,10 +1,18 @@
 package torrent
 
 import (
+	"errors"
 	"github.com/anacrolix/torrent"
+	"github.com/anacrolix/torrent/metainfo"
 	"github.com/majestrate/bitchan/gossip"
 	"github.com/majestrate/bitchan/storage"
+	"github.com/sirupsen/logrus"
+	"net/http"
 )
+
+var log = logrus.New()
+
+var ErrNotAdded = errors.New("torrent not addded")
 
 type Grabber struct {
 	client   *torrent.Client
@@ -12,12 +20,51 @@ type Grabber struct {
 	store    storage.Store
 }
 
+func (g *Grabber) Grab(metainfoURL string) error {
+	log.WithFields(logrus.Fields{
+		"url": metainfoURL,
+	}).Info("grabbing metainfo")
+	resp, err := http.Get(metainfoURL)
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"url": metainfoURL,
+			"err": err,
+		}).Error("grabbing metainfo failed")
+		return err
+	}
+	defer resp.Body.Close()
+	mi, err := metainfo.Load(resp.Body)
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"url": metainfoURL,
+			"err": err,
+		}).Error("reading metainfo failed")
+		return err
+	}
+	t, err := g.client.AddTorrent(mi)
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"url": metainfoURL,
+			"err": err,
+		}).Error("failed to add torrent")
+		return err
+	}
+
+	log.WithFields(logrus.Fields{
+		"url": metainfoURL,
+	}).Info("download starting")
+	t.DownloadAll()
+	return nil
+}
+
 func (g *Grabber) Stop() {
 	g.client.Close()
 }
 
 func NewGrabber(st storage.Store, g gossip.Gossiper) *Grabber {
-	t, _ := torrent.NewClient(torrent.NewDefaultClientConfig())
+	cfg := torrent.NewDefaultClientConfig()
+	cfg.DataDir = st.GetRoot()
+	t, _ := torrent.NewClient(cfg)
 	return &Grabber{
 		client:   t,
 		gossiper: g,
